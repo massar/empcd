2d5f13f350d8acfd60913057aa1036bc9c11182f
Jeroen Massar <jeroen@massar.ch>
2014-01-08 12:48:31 +0100

    Constant strings are a good thing

--- support/mpc-0.12.2/src/libmpdclient.c
+++ support/mpc-0.12.2/src/libmpdclient.c
@@ -238,7 +238,7 @@ static int mpd_connect(mpd_Connection * connection, const char * host, int port,
 }
 #endif /* !MPD_HAVE_GAI */
 
-char * mpdTagItemKeys[MPD_TAG_NUM_OF_ITEM_TYPES] =
+const char * mpdTagItemKeys[MPD_TAG_NUM_OF_ITEM_TYPES] =
 {
 	"Artist",
 	"Album",
@@ -426,11 +426,11 @@ void mpd_closeConnection(mpd_Connection * connection) {
 	WSACleanup();
 }
 
-static void mpd_executeCommand(mpd_Connection * connection, char * command) {
+static void mpd_executeCommand(mpd_Connection * connection, const char * command) {
 	int ret;
 	struct timeval tv;
 	fd_set fds;
-	char * commandPtr = command;
+	const char * commandPtr = command;
 	int commandLen = strlen(command);
 
 	if(!connection->doneProcessing && !connection->commandList) {
@@ -1639,7 +1639,7 @@ void mpd_startSearch(mpd_Connection *connection, int exact)
 
 void mpd_startFieldSearch(mpd_Connection *connection, int type)
 {
-	char *strtype;
+	const char *strtype;
 
 	if (connection->request) {
 		strcpy(connection->errorStr, "search already in progress");
@@ -1663,7 +1663,7 @@ void mpd_startFieldSearch(mpd_Connection *connection, int type)
 
 void mpd_addConstraintSearch(mpd_Connection *connection, int type, const char *name)
 {
-	char *strtype;
+	const char *strtype;
 	char *arg;
 
 	if (!connection->request) {
